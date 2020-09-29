4f315fd03c775bffed1eb5d0c8075602b4c089c7
Abhishek <abhishek.orion@gmail.com>
2020-09-29 11:59:35 +0530

    Removed unused code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React from 'react';
 import Container from '@material-ui/core/Container';
 import ThoughtPad from './containers/ThoughtPad'
 
